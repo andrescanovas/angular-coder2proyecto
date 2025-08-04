aa974a6d5bb615e216f3a7d3b6ba0bfc997a6cc1
Andres Canovas <andrescanovas92@gmail.com>
2025-08-04 19:13:33 -0300

    visualizacion de tabla alumnos

--- src/app/features/alumnos/alumnos.ts
+++ src/app/features/alumnos/alumnos.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { AlumnosAPI } from './alumnos-api';
+import { Student } from '../../../shared/entities';
+import { CommonModule } from '@angular/common';
+import { StudentTable } from "../../student-table/student-table";
 
 @Component({
   selector: 'app-alumnos',
-  imports: [],
+  imports: [CommonModule, StudentTable],
   templateUrl: './alumnos.html',
   styleUrl: './alumnos.scss'
 })
 export class Alumnos {
+alumnos! :Student[];
+  constructor(private alumnosApi: AlumnosAPI) { }
+
+  ngOnInit() {
+   
+    this.alumnosApi.getAlumnos().subscribe(alumnos => {
+       this.alumnos = alumnos;
+    });
+
+   
+  }
 
 }
